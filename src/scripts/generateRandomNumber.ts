4fac46ff822d801426061739367d7cee36815e28
Wanderlee Max <wanderleedev@gmail.com>
2024-01-01 11:17:23 -0500

    refactor

--- src/scripts/generateRandomNumber.ts
+++ src/scripts/generateRandomNumber.ts
@@ -1,3 +1,16 @@
+import { banners } from "@data/banners";
+
 export function generateRandomNumber (numberRange: number): number {
   return Math.floor(Math.random() * numberRange);
+}
+
+export function generateRandomBanner (): string {
+  return banners[generateRandomNumber(banners.length)];
+}
+
+export function applyBanner (): void {
+  const banner = document.querySelector('#banner') as HTMLDivElement;
+  if (banner instanceof HTMLDivElement) {
+    banner.style.backgroundImage = `url(${generateRandomBanner()})`
+  }
 }
